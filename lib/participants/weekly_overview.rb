df17738fc6ecb08c6823895caa64a800cbeb6cc8
Alan <a-ashbeck@northwestern.edu>
2016-05-18 12:00:39 -0500

    Refactor weekly_overview.rb

--- lib/participants/weekly_overview.rb
+++ lib/participants/weekly_overview.rb
@@ -14,7 +14,7 @@ class Participant
     end
 
     def click_chapter_cycle
-      num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+      num = (0..9).map(&:to_s)
 
       num.each do |i|
         find("#lesson-#{i}").click
--- spec/features/web_session_spec.rb
+++ spec/features/web_session_spec.rb
@@ -19,7 +19,7 @@ feature 'Participant selects web session' do
   scenario 'and completes session 1' do
     participant_1.sign_in
     web_session.click_web_session
-    sleep(0.5)
+    sleep(0.1)
     participant_1.scroll_down
     web_session.fill_form_pre_web_session
     web_session.click_go_to_web_session
