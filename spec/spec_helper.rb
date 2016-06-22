7aad75194edec54c9b0bea590a5362bca927ae4a
Alan <a-ashbeck@northwestern.edu>
2016-06-22 15:32:49 -0500

    Add Poltergest

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@
 require 'rspec'
 require 'capybara'
 require 'capybara/rspec'
+require 'capybara/poltergeist'
 require 'capybara-screenshot/rspec'
 require 'selenium-webdriver'
 
@@ -34,9 +35,16 @@ Capybara.configure do |config|
     Selenium::WebDriver::Firefox::Binary.path = ENV['Firefox_Path']
     Capybara::Selenium::Driver.new(app, browser: browser)
   end
-  config.default_driver = :selenium
-  config.page.driver.browser.manage.window.resize_to(1280, 743)
-  config.save_and_open_page_path = 'spec/screenshots/'
+  config.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, js: true)
+  end
+  driver = ENV['driver'].nil? ? :selenium : ENV['driver'].to_sym
+  config.default_driver = driver
+  unless ENV['driver'] == 'poltergeist'
+    config.page.driver.browser.manage.window.resize_to(1280, 743)
+  end
+  config
+    .save_path = "#{ENV['Path']}/conemo_dashboard_features/spec/screenshots/"
 end
 
 # capybara-screenshot configuration options
